13c676a3ba9153f46b576f01957201b8040c9e59
Laurel McLean <laurel.mclean@live.ca>
2024-08-18 12:45:26 -0600

    updating folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import About from "./components/About";
+import About from "./components/About/About";
 import './App.css';
-import Navbar from "./components/Nav";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
+import Navbar from "./components/Nav/Nav";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
