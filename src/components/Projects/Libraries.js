89efd4a9ce6f495f3d6008a62f462b5382216ed3
Laurel McLean <laurel.mclean@live.ca>
2024-09-03 14:47:18 -0600

    progress on npm card styling

--- src/components/Projects/Libraries.js
+++ src/components/Projects/Libraries.js
@@ -1,31 +1,31 @@
 import React from "react";
-import "./Projects.css"
+import "./Libraries.css"
 import { libraries } from "../../data";
 
 export default function Libraries() {
     return (
-        <section id="projects">
-            <div className="projects-container">
-                <div className="projects-header">
-                    <h1 className="projects-title">
+        <section id="libraries">
+            <div className="libraries-container">
+                <div className="libraries-header">
+                    <h1 className="libraries-title">
                         NPM Packages
                     </h1>
-                    <p className="projects-description">
+                    <p className="libraries-description">
                         Here are a collection of NPM packages I've created and shared with the global developer community on npmjs.com
                     </p>
                 </div>
-                <div className="projects-grid">
-                    {libraries.map((project) => (
+                <div className="libraries-grid">
+                    {libraries.map((library) => (
                         <a
-                            href={project.link}
-                            key={project.link}
-                            className="project-item">
-                            <div className="project-card">
-                                <div className="project-info">
-                                    <h1 className="project-title">
-                                        {project.title}
+                            href={library.link}
+                            key={library.link}
+                            className="library-item">
+                            <div className="library-card">
+                                <div className="library-info">
+                                    <h1 className="library-title">
+                                        {library.title}
                                     </h1>
-                                    <p className="project-description">{project.description}</p>
+                                    <p className="library-description">{library.description}</p>
                                 </div>
                             </div>
                         </a>
